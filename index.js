b10e33c2393815ce1ddb91de52572d06b9c886f7
Roman <mr.dukuy@gmail.com>
2024-10-20 15:39:43 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const bookList = [
+  {
+    title: "From Third World To First: The Singapore Story: 1965-2000",
+    url: "https://dykyi-roman.github.io/book-review/from_third_world_to_first-the_singapore_story_1965_2000.html"
+  },
+  {
+    title: "Atlas Shrugged",
+    url: "https://dykyi-roman.github.io/book-review/atlas_shrugged.html"
+  },
+  {
+    title: "Sapiens: A Brief History of Humankind",
+    url: "https://dykyi-roman.github.io/book-review/sapiens-a_brief_history_of_humankind.html"
+  }
+];
+
+function loadBooks() {
+  const bookListElement = document.querySelector('.book-list');
+
+  bookList.forEach(book => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.className = 'white-text';
+    a.href = book.url;
+    a.textContent = book.title;
+
+    li.appendChild(a);
+    bookListElement.appendChild(li);
+  });
+}
+
+window.onload = loadBooks;
